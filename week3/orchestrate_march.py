68194f866adf2735f71501e2cbd7b2f3f25a3f1b
mruiyangyou <m.ruiyangyou@gmail.com>
2023-06-05 21:21:17 +0800

    change data input

--- week3/orchestrate_march.py
+++ week3/orchestrate_march.py
@@ -1,3 +1,5 @@
+from prefect.artifacts import create_markdown_artifact
+from datetime import date
 import pathlib
 import pickle
 import pandas as pd
@@ -106,13 +108,31 @@ def train_best_model(
         mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
 
         mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+        
+        markdown__rmse_report = f"""# RMSE Report
+
+        ## Summary
+
+        Duration Prediction 
+
+        ## RMSE XGBoost Model
+
+        | Region    | RMSE |
+        |:----------|-------:|
+        | {date.today()} | {rmse:.2f} |
+        """
+
+        create_markdown_artifact(
+            key="march-taxi-report", markdown=markdown__rmse_report
+        )
+
     return None
 
 
 @flow
 def main_flow(
-    train_path: str = "./data/green_tripdata_2023-01.parquet",
-    val_path: str = "./data/green_tripdata_2023-02.parquet",
+    train_path: str = "./data/green_tripdata_2023-02.parquet",
+    val_path: str = "./data/green_tripdata_2023-03.parquet",
 ) -> None:
     """The main training pipeline"""
 
